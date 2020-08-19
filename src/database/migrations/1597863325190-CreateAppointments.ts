874388afdbdbb06acd9edb462f8ca646d9d45832
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-19 18:24:56 -0300

    🚀refactor:  refactor Appointments using Typeorm

--- src/database/migrations/1597863325190-CreateAppointments.ts
+++ src/database/migrations/1597863325190-CreateAppointments.ts
@@ -8,19 +8,18 @@ export class CreateAppointments1597863325190 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
           {
             name: 'created_at',
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+
 import express, { Request, Response, NextFunction } from 'express';
 
 import routes from './routes';
