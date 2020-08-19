122a4fd90e8bc19eb0d2f6e355b2fff1ea481c5e
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-19 16:08:06 -0300

    🚨 Feat: Added Error handler on server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import routes from './routes';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -9,6 +10,22 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
