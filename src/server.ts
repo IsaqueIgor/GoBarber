a1b84bcf610603f535ff75532e40331320421a25
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-23 16:56:06 -0300

    feat: update User Avatar

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
-import 'reflect-metadata';
+import 'reflect-metadata'; // Allows us to do runtime reflection on types.
 
 import express, { Request, Response, NextFunction } from 'express';
 
 import routes from './routes';
 import AppError from './errors/AppError';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
